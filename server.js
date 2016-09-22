04f6ea7477e6df369211332e3ec6660be63d7a8c
Artiik <artem-makovetskiy@mail.ru>
2016-09-22 10:27:00 +0000

    created simple server and dir structure

---
+++ server.js
@@ -0,0 +1,15 @@
+
+// grab dependencies
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 8080;
+
+// configure application
+
+// set routes
+app.use(require('./app/routes'));
+
+// start server
+app.listen(port, () => {
+    console.log(`App listening on http://node-crud:${port}`);
+});
